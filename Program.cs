a914028b54f0c9b3eaacf832db6f76bdbff13e97
Axel <axel.lefort@hotmail.fr>
2020-10-05 14:05:35 +0200

    added common libs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solution_Shifu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
